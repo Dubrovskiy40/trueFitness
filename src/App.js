889731634ab48a2603ddfd12eec8c1f2875ceab0
Alexsey215 <orlov200015@gmail.com>
2022-03-08 15:48:26 +0700

    Сделал header на главной странице

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from "./components/footer/Footer";
 import RequestForm from "./components/request-form";
+import Header from "./components/header";
 
 function App(props) {
   return (
     <div className="App">
+        <Header />
       <header className="App-header">
         <p>
           Place for our future True Fitness app =)
