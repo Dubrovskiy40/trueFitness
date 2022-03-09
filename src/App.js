0a80c3b1a1118f5e58e4179c1c2ae74ec0199225
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-03-09 21:27:51 +0300

    добавил блоки main и about

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import CarouselComp from './components/Carousel/Carousel';
 import Footer from "./components/footer/Footer";
 import RequestForm from "./components/request-form";
 import Header from "./components/header";
+import Main from "./components/main/Main";
+import AboutUs from "./components/aboutUs/AboutUs";
 
 function App(props) {
   return (
     <div className="App">
       <Header />
+      <Main />
       <CarouselComp slider='Тренировки' />
+      <AboutUs />
       <CarouselComp slider='Рецепты' />
       <RequestForm />
       <Footer />
