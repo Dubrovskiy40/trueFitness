3d639879fadbbd841c7fd9d8edb233801881d606
Alexsey215 <orlov200015@gmail.com>
2022-05-03 19:08:23 +0700

    Pagination

--- frontend/src/components/Account/AccountAchievments/index.js
+++ frontend/src/components/Account/AccountAchievments/index.js
@@ -1,7 +1,7 @@
 export default () => {
-  return (
-    <div>
-      <h1>Достижения</h1>
-    </div>
-  )
+      return (
+        <div>
+            <h1>Достижения</h1>
+        </div>
+      )
 }
---
+++ frontend/src/components/Pagination/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
+import style from './index.module.scss'
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }) {
+    return (
+        <>
+            {currentItems &&
+            currentItems.map((item) => (
+                <div>
+                    {item.text}
+                </div>
+            ))}
+        </>
+    );
+}
+
+function PaginatedItems({ itemsPerPage }) {
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+        // Fetch items from another resources.
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(items.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(items.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % items.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            <Items currentItems={currentItems}/>
+            <ReactPaginate
+                className={style.paginate}
+                activeClassName={style.paginate__active}
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+            />
+        </>
+    );
+}
+
+export default PaginatedItems;
