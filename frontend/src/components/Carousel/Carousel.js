43ae9ae3c61eb9f336318c33527c11ae8b861268
Evgeniy Novakovskiy <allidon93@gmail.com>
2022-05-13 21:23:59 +0700

    Added commented "more" card into both carousels xD

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -1,10 +1,14 @@
 import { Carousel } from '@trendyol-js/react-carousel';
 import './carousel.css';
+import style from '../CardItem/cardItem.module.scss';
 import CarouselArrowLeft from '../CarouselArrows/CarouselArrowLeft';
 import CarouselArrowRight from '../CarouselArrows/CarouselArrowRight';
 import CardItem from "../CardItem";
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import moreTrainingImg from '../../images/carousel/more-trainings-preview.png'
+import moreRecipesImg from '../../images/carousel/more-recipes-preview.png'
 
 const CarouselComp = (props) => {
   const [data, setData] = useState();
@@ -39,6 +43,18 @@ const CarouselComp = (props) => {
         {data.slice(0, cardsToShow).map((item, idx) => (
           <CardItem item={item} key={idx} category={props.category} />
         ))}
+        {/* <div className={style.card__item}>
+          <Link to={props.category === 'trainings' ? `${props.category}` : `recipes`}>
+            <img
+              src={props.category === 'trainings' ? moreTrainingImg : moreRecipesImg}
+              className={style.card__item__img}
+              alt=""
+            />
+            <h3 className={style.card__item__heading + ' heading--center'}>
+              Ещё...
+            </h3>
+          </Link>
+        </div> */}
       </Carousel>}
     </div >
   )
